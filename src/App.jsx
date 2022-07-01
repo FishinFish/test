6455dcecea46096d19c5a50b8bd86efff0abd615
FishinFish <tony_523432553@qq.com>
2022-07-01 21:26:52 +0800

    update all

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-
+import {Route,Switch,Redirect} from 'react-router-dom'
+import MyNavLink from './components/MyNavLink'
+import Home from './pages/Home'
+import About from './pages/About'
 export default class App extends Component {
   render() {
     return (
       <div>
         <h2>React 路由练习</h2>
         <div>
-
+            <MyNavLink to = "/home">Home</MyNavLink>
+            <MyNavLink to = "/about">About</MyNavLink>
         </div>
         <div>
-
+            <Switch>
+              <Route path = "/home" component={Home}/>
+              <Route path = "/about" component={About}/>
+              <Redirect to = "/home"/>
+            </Switch>
         </div>
       </div>
     )
