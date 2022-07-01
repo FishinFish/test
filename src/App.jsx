5483b2b4e1feef6f0adc0a4102da0131a152e501
FishinFish <tony_523432553@qq.com>
2022-07-01 21:07:13 +0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>React 路由练习</h2>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+}
